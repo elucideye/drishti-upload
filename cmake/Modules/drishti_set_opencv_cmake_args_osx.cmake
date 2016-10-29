996bfecceddd5680a12f04222e2fdb8b5fa28b77
David Hirvonen <dhirvonen@elucideye.com>
2016-10-29 09:12:23 -0400

    exclude unused TIFF and JPEG from platform builds

--- cmake/Modules/drishti_set_opencv_cmake_args_osx.cmake
+++ cmake/Modules/drishti_set_opencv_cmake_args_osx.cmake
@@ -17,9 +17,9 @@ macro(drishti_set_opencv_cmake_args_osx)
 
       ### Custom ARGS ###
       WITH_PNG=ON             # "Include PNG support"
-      WITH_TIFF=ON            # "Include TIFF support"
+      WITH_TIFF=OFF           # "Include TIFF support"
       WITH_JASPER=ON          # "Include JPEG2K support"
-      WITH_JPEG=OFF            # "Include JPEG support"
+      WITH_JPEG=OFF           # "Include JPEG support"
       BUILD_JPEG=OFF
 
       WITH_OPENCL=NO
