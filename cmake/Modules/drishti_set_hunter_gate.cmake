210f3d84745c3be13bbacc96b354e19f5722d649
David Hirvonen <dhirvonen@elucideye.com>
2016-11-13 14:34:13 -0500

    add shared macro for setting hunter version and sha1

---
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -0,0 +1,4 @@
+macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.16.29.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "309d7d8eedc5b1229b710f856dc7a19e04fb7737")
+endmacro()
