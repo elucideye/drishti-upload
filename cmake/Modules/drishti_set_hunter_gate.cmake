faf81980d499abbcf597168d37d5369953d67d1c
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-12-03 19:17:46 +0800

    Update Hunter

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL}  "https://github.com/ruslo/hunter/archive/v0.16.34.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "9a7b420de4b288020b02f68d8aa827c0959a52bf")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.16.35.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "3c8f029374d801aa7a4677ef262e38d4432eaf17")
 endmacro()
