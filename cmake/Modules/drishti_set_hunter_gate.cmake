534076d19548345901669df7b089a81b4ca633ed
David Hirvonen <dhirvonen@elucideye.com>
2016-12-05 01:28:52 -0500

    update hunter to v0.16.36; update ogles_gpgpu to 0.1.6-p1

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.16.35.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "3c8f029374d801aa7a4677ef262e38d4432eaf17")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.16.36.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "a47baf7a23fee038812baa7d6a9362ca1770b6ed")
 endmacro()
