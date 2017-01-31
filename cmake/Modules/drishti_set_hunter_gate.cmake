ff94732c56074d81a5529d6b69e6efaefe6f8747
David Hirvonen <dhirvonen@elucideye.com>
2017-01-30 23:12:58 -0500

    use upstream hunnter

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/headupinclouds/hunter/archive/x17.01.30.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "34e928b462d14d5d5f27d48ced8b6c19f16263d6")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.17.18.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "ffd57842beb628402ea918508718c6f84abe3771")
 endmacro()
