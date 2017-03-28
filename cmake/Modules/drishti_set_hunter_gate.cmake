c4568044483ba23d488715b7a2ae6134abfdbae6
David Hirvonen <dhirvonen@elucideye.com>
2017-03-28 10:34:46 -0400

    typo

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,5 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
   set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.27.tar.gz")
   set(${DRISHTI_HUNTER_GATE_SHA1} "dcc5416d24b536856c780edc1ba7cc3087487eeb")
-)
 endmacro()
