4df4e70756960e2fca9f6b5fb5fbe9eef1eaed46
David Hirvonen <dhirvonen@elucideye.com>
2017-04-02 15:59:05 -0400

    hunter=v0.18.35 | add: sse2neon; ARM_NEON_2_x86_SSE; glfw (#60)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.28.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "065fa6d2d62fde2bfbcd936b2a2ebd26a65a5529")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.35.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "8f75b22def52aa6918f12e66c0617306c9d3af6f")
 endmacro()
