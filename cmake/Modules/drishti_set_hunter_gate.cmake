415663da1429b70447ee3d55a84573ac0cc7aa00
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-05-14 14:53:15 +0300

    Update Hunter (Android emulator)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.48.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "71ec0afd26f7df84daa1e3e7d0da4d42f6072504")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.55.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "0dcfb305e43e492bec485b7404827b3b5dd4d2e6")
 endmacro()
