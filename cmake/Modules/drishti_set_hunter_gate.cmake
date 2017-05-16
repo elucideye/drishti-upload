f4d73ed0cdb7327ff37fed2f2d7ce7bc14bf184e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-05-16 14:11:51 +0300

    Use 'hunter_config(GIT_SUBMODULE)' feature

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.55.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "0dcfb305e43e492bec485b7404827b3b5dd4d2e6")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.58.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "3b39effc5ee1af4ef7487eabb1b0a7a7e10a7b3e")
 endmacro()
