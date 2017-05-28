c407686a762783a41a6c720d5286cc96c4576f8b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-05-28 23:59:54 +0300

    Update Hunter (cellar feature)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.18.59.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "40940b1b346f55505b3c2bb856f3bf100400d613")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.0.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "e9d11aee66607cfcfbec5e3811d4804135b5bfac")
 endmacro()
