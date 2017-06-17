bcdfe2e6444726ce9802c3fc2a2a25cba7b9a642
David Hirvonen <dhirvonen@elucideye.com>
2017-06-16 23:19:47 -0400

    update hunter version; add glm to cache (#79)
    * add Beast version (dev use only, don’t add to cache yet)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.10.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "73b5a5253b75d5ed79c89242c167ca1f94386621")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.14.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "d4216c82be9c273ce2567b35f75356da045b884f")
 endmacro()
