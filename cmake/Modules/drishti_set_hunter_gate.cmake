2503ecb6688feafea0deb4488897f4ec157eb21f
David Hirvonen <dhirvonen@elucideye.com>
2017-08-09 19:12:19 -0400

    add temporary hunter for json android fix

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,5 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.63.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "fdd2277a60794ff641e8d8f9b15d3a0c2084c73d")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/headupinclouds/hunter/archive/v0.19.63-dev.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "4a8247c82b5578d6c6fe7695c571d40acb68d4db")
 endmacro()
+
