397fd4e9c2eff76384baa81d21aca604c590f88f
David Hirvonen <dhirvonen@elucideye.com>
2017-08-09 07:43:34 -0400

    "cereal only" mods {DON'T MERGE} (#90)
    "cereal only" mods

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.51.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "d238dc1dd4db83e45a592f96fdb95d17c688600a")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.62.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "e15b764e94641968fe88db08dfb8e7f0f084c01c")
 endmacro()
