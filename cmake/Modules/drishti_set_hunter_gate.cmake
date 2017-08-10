9ed8a01aa6b678a156bdb2e4e753a0e5ba90a6b9
David Hirvonen <dhirvonen@elucideye.com>
2017-08-09 20:16:10 -0400

    update hunter; json android workaround (#91)
    * update hunter use patched nlohmann-json
    
    * add temporary hunter for json android fix

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,5 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.62.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "e15b764e94641968fe88db08dfb8e7f0f084c01c")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/headupinclouds/hunter/archive/v0.19.63-dev.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "4a8247c82b5578d6c6fe7695c571d40acb68d4db")
 endmacro()
+
