c7d44a60a22bfddaa7fece0aff579980149eb556
David Hirvonen <dhirvonen@elucideye.com>
2017-08-15 10:53:13 -0400

    update hunter; replace hunter GIT_SUBMODULEs with packages

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,5 +1,5 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.64.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "50a591883dba8e6dafcfba412a03024c2b1a27a7")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.70.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "739aa8f0aba28b6fee6862885a8a6874a37d09b6")
 endmacro()
 
--- config.cmake
+++ config.cmake
@@ -44,7 +44,7 @@ list(APPEND OPENCV_CMAKE_ARGS
   WITH_OPENCL=OFF
   BUILD_EIGEN=OFF  ### for convenient linking
   BUILD_SHARED_LIBS=OFF
-  )
+)
 
 set(drishti_boost_version 1.64.0)
 
@@ -54,18 +54,33 @@ set(EIGEN_CMAKE_ARGS
   BUILD_TESTING=OFF
   HUNTER_INSTALL_LICENSE_FILES=COPYING.MPL2
   CMAKE_Fortran_COMPILER=OFF
-  )
+)
 
-# Set xgboost args
+### Set xgboost args ###
 set(XGBOOST_CMAKE_ARGS
+  XGBOOST_USE_CEREAL=ON  
   XGBOOST_USE_HALF=ON
-  XGBOOST_USE_CEREAL=ON
-  )
+)
+
 if(DRISHTI_BUILD_MIN_SIZE)
   list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=ON)
 else()
   list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
 endif()
+
+### Toggle OpenGL ES 3.0 ###
+if(DRISHTI_OPENGL_ES3)
+  set(use_opengl_es3 ON)
+else()
+  set(use_opengl_es3 OFF)
+endif()
+
+set(OGLES_GPGPU_CMAKE_ARGS
+  OGLES_GPGPU_VERBOSE=OFF
+  OGLES_GPGPU_OPENGL_ES3=${use_opengl_es3}
+)
+
+set(AGLET_CMAKE_ARGS AGLET_OPENGL_ES3=${use_opengl_es3})
 
 if(APPLE OR ${is_linux} OR MSVC)
   hunter_config(Jpeg VERSION 9b-p1)
@@ -79,14 +94,20 @@ hunter_config(OpenCV VERSION 3.0.0-p11 CMAKE_ARGS "${OPENCV_CMAKE_ARGS}")
 hunter_config(PNG VERSION 1.6.26-p1)
 hunter_config(Qt VERSION 5.5.1-cvpixelbuffer-2-p9)
 hunter_config(RapidXML VERSION 1.13)
+hunter_config(aglet VERSION 1.2.0)
 hunter_config(cereal VERSION 1.2.1-p1)
 hunter_config(cvmatio VERSION 1.0.27-p3)
 hunter_config(dlib VERSION 19.2-p1)
+hunter_config(drishti_assets VERSION 1.7)
+hunter_config(drishti_faces VERSION 1.2)
 hunter_config(glfw VERSION 3.3.0-p1)
 hunter_config(half VERSION 1.1.0-p1)
 hunter_config(nlohmann_json VERSION 2.1.1-p1)
+hunter_config(ogles_gpgpu VERSION 0.2.1)
 hunter_config(spdlog VERSION 0.13.0-p0)
 hunter_config(sse2neon VERSION 1.0.0-p0)
+hunter_config(thread-pool-cpp VERSION 1.1.0)
+hunter_config(xgboost VERSION 0.40-p9)
 
 # experimental: lock verison but not used for CI builds
 hunter_config(dest VERSION 0.8.0-p4)
@@ -97,28 +118,6 @@ hunter_config(tinydir VERSION 1.2-p0)
 hunter_config(Beast VERSION 1.0.0-b32-hunter-4)
 
 if(NOT DRISHTI_UPLOAD_IGNORE_SUBMODULES)
-
-  if(DRISHTI_OPENGL_ES3)
-    set(use_opengl_es3 ON)
-  else()
-    set(use_opengl_es3 OFF)
-  endif()
-
-  set(OGLES_GPGPU_CMAKE_ARGS
-    OGLES_GPGPU_VERBOSE=OFF
-    OGLES_GPGPU_OPENGL_ES3=${use_opengl_es3})
-
-  set(AGLET_CMAKE_ARGS
-    AGLET_OPENGL_ES3=${use_opengl_es3})
-
-  # Use submodules for first packaged version:
-  hunter_config(drishti_assets GIT_SUBMODULE "src/3rdparty/drishti-assets")
-  hunter_config(drishti_faces GIT_SUBMODULE "src/3rdparty/drishti-faces")
-  hunter_config(aglet GIT_SUBMODULE "src/3rdparty/aglet" CMAKE_ARGS ${AGLET_CMAKE_ARGS})
-
-  hunter_config(ogles_gpgpu GIT_SUBMODULE "src/3rdparty/ogles_gpgpu" CMAKE_ARGS ${OGLES_GPGPU_CMAKE_ARGS})
-  hunter_config(xgboost GIT_SUBMODULE "src/3rdparty/xgboost" CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
-  hunter_config(thread-pool-cpp GIT_SUBMODULE "src/3rdparty/thread-pool-cpp")
 
   # Note: MSVC currently broken due to internal GL_BGR(A) enums
   # TODO: Update imshow package
