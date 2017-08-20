e4a167ce776b5b3bea0685c1cce5c0713f8a136b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-20 10:32:26 +0300

    Update Hunter (Qt 5.9)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.74.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "a66589c041948aec2bcf374b7a45fd1e10bbc6b1")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.75.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "35b76b78b85bcf1dce81e0cef32cc1f728170abf")
 endmacro()
