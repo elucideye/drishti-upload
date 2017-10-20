722455da75f03841191655bf0b628e2e6f826450
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-10-20 15:09:36 +0300

    Update Hunter (Gauze 0.1.2)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.123.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "57d07480686f82ddc916a5980b4f2a18e5954c2b")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.127.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "d685a47797ab9fd724b910c67505765cf1fe402a")
 endmacro()
