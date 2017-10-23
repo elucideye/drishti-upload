02e9180e045fb80167bb2ec6a6b3d947ee972b13
David Hirvonen <dhirvonen@elucideye.com>
2017-10-22 23:33:33 -0400

    add eos to cmake and eos deps to config
    * update hunter
    * add hunter_add_package for eos + deps
    * bump version of eigen3-nnls (gcc fix)
    * add explicit nanoflann version

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.129.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "3e190ff47df41cc16d32c479b9617309e01405bf")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.131.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "bc0eec34712af8854085c2dc6a90b78df17a9743")
 endmacro()
