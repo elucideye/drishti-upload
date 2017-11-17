6476ae4018af6833e2915cc080cab753433e270f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-11-17 22:23:24 +0300

    Update Hunter (Android SDK optimization)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.131.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "bc0eec34712af8854085c2dc6a90b78df17a9743")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.161.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "9af482b6a54875bbbb221274f7290324dff29919")
 endmacro()
