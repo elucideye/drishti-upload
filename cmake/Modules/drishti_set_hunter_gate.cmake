0b04543a83aff9958f9041c5a1215c1c6187a36a
David Hirvonen <dhirvonen@elucideye.com>
2017-11-19 21:55:36 -0500

    hunter v0.19.165 (#116)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.161.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "9af482b6a54875bbbb221274f7290324dff29919")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.165.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "61153ee77b9a3651aae3611d57c3497b28001194")
 endmacro()
