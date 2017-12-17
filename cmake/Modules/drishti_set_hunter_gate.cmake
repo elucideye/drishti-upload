75723beaef73d04b319fa6b11a848b8c92714956
David Hirvonen <dhirvonen@elucideye.com>
2017-12-17 18:49:58 -0500

    use dlib as a library (not header only) (#121)
    * use dlib as a library (not header only)
    
    * use dlib as standard installed library (package config) per davis king’s online recommendations
    * update to hunter v0.19.201
    
    * clang-3.9
    
    https://stackoverflow.com/a/47003934
    
    * default gcc ; symlinks for clang
    
    * use hunter fork for dlib 19.6 chrono patch
    
    * use patched dlib
    
    * update hunter release

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.178.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "1b94f895831f76e5a63e0537dd366ae142543c02")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/elucideye/hunter/archive/2017.12.17.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "7a09cb944a22cbca0b9e133cab638538e0e37e29")
 endmacro()
