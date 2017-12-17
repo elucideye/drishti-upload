752ce8bc6d85581f11b23a7ef1a4cce178396fb6
David Hirvonen <dhirvonen@elucideye.com>
2017-12-17 17:17:54 -0500

    use hunter fork for dlib 19.6 chrono patch

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.201.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "50b87e69d3903c0b4d1d34609333d6ca93479e7a")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/elucideye/hunter/archive/v2017.12.17.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "32858b102485865110edbd03965308bf85eb529f")
 endmacro()
