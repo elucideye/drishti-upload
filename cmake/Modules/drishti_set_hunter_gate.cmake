b3da050792447be657f7c79105685b183dd13476
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-01-25 23:08:33 +0300

    Update Hunter (ogles_gpgpu 0.2.8)

--- cmake/Modules/drishti_set_hunter_gate.cmake
+++ cmake/Modules/drishti_set_hunter_gate.cmake
@@ -1,4 +1,4 @@
 macro(drishti_set_hunter_version DRISHTI_HUNTER_GATE_URL DRISHTI_HUNTER_GATE_SHA1)
-  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.203.tar.gz")
-  set(${DRISHTI_HUNTER_GATE_SHA1} "b4b02cd58f3f6e2945c5e01f3c405c929b849b3c")
+  set(${DRISHTI_HUNTER_GATE_URL} "https://github.com/ruslo/hunter/archive/v0.19.235.tar.gz")
+  set(${DRISHTI_HUNTER_GATE_SHA1} "c8d6fca16c83fec2e6e6fa2e92455bad0045c0f4")
 endmacro()
